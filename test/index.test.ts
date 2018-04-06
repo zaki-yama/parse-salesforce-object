0bbebef29e8f4cbc21c63e3587c31652ee8267ba
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-04-06 10:17:51 +0900

    #4 Add test with no option

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,9 +5,15 @@ import cmd = require('../src')
 describe('parse-salesforce-object', () => {
   test
   .stdout()
-  .do(() => cmd.run([]))
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
+  .do(() => cmd.run(['./test/Expense__c.object']))
+  .it('formats as Markdown table when giving no option', ctx => {
+    const expected = `| label       | fullName      | type     | required |
+| ----------- | ------------- | -------- | -------- |
+| Amount      | Amount__c     | Number   | false    |
+| Client      | Client__c     | Text     | false    |
+| Date        | Date__c       | DateTime | false    |
+| Reimbursed? | Reimbursed__c | Checkbox | null     |`
+    expect(ctx.stdout).to.contain(expected)
   })
 
   test
