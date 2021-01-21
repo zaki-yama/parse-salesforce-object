4d9ea9a3e714e7068d3a41e6186ecb8b0fdb2323
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-01-21 21:40:39 +0900

    chore(deps): Update oclif (#17)
    * chore(deps): update oclif
    
    * chore: temporarily disable unicorn/no-abusive-eslint-disable
    
    * chore(build): add lint script
    
    * fix: suppress typescript error
    
    * chore: delete unnecessary files
    
    * chore: restore keyword

--- src/index.ts
+++ src/index.ts
@@ -84,32 +84,36 @@ class ParseSalesforceObject extends Command {
         }
         dataList.push(data)
       }
+    // eslint-disable-next-line unicorn/catch-error-name
     } catch (err) { // tslint:disable-line no-unused
-        this.error('ERROR: Invalid XML format.')
+      this.error('ERROR: Invalid XML format.')
     }
 
     let result
     switch (flags.format) {
-      case 'soql':
-        const prefix = flags.namespace ? `${flags.namespace}__` : ''
-        const objName = path.basename(args.path).split('.')[0]
-        const fields = dataList.map(data => {
-          return `${prefix}${data.fullName}`
-        })
-        result = [
-          'SELECT Id,',
-          fields.join(',\n'),
-          `FROM ${prefix}${objName}`,
-        ].join('\n')
-        break
-      case 'csv':
-        result = parse(dataList, {fields: props})
-        break
-      default:
-        // markdown
-        result = table([props, ...dataList.map(data => Object.values(data))], {
-          stringLength: stringWidth
-        })
+    case 'soql': {
+      const prefix = flags.namespace ? `${flags.namespace}__` : ''
+      const objName = path.basename(args.path).split('.')[0]
+      const fields = dataList.map(data => {
+        return `${prefix}${data.fullName}`
+      })
+      result = [
+        'SELECT Id,',
+        fields.join(',\n'),
+        `FROM ${prefix}${objName}`,
+      ].join('\n')
+      break
+    }
+    case 'csv':
+      result = parse(dataList, {fields: props})
+      break
+    default:
+      // markdown
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      result = table([props, ...dataList.map(data => Object.values(data))], {
+        stringLength: stringWidth,
+      })
     }
     this.log(result)
   }
